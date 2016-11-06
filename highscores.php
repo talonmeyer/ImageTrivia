34c032a41484a717a7877d945243e05ab70f1c20
Talon Meyer <talon@iastate.edu>
2016-11-05 23:24:13 -0500

    Applied CSS styles to sub pages

--- highscores.php
+++ highscores.php
@@ -1,68 +1,28 @@
 <?php
-include('session.php');
+    include('session.php');
 ?>
 
-<html>
+    <html>
+       <body bgcolor = "#FFFFFF">
+       <head>
+           <link rel="stylesheet" type="text/css" href="css/style.css">
+       </head>
 
-<body bgcolor = "#FFFFFF">
+       <div class="main-menu">
+           <div class="form">
+               <p class="message" style="color:black; font-size:medium ">Board of Champions</p>
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-
-<head>
-   <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<div class="table-page">
-   <div class="form">
-
-      <p class="message" style="color:black; font-size:medium ">Board of Champions</p>
-
-      <br> </br>
-
-       table class="pure-table">
-       <thead>
-       <tr>
-           <th>#</th>
-           <th>Make</th>
-           <th>Model</th>
-           <th>Year</th>
-       </tr>
-       </thead>
-
-       <tbody>
-       <tr>
-           <td>1</td>
-           <td>Honda</td>
-           <td>Accord</td>
-           <td>2009</td>
-       </tr>
-
-       <tr>
-           <td>2</td>
-           <td>Toyota</td>
-           <td>Camry</td>
-           <td>2012</td>
-       </tr>
-
-       <tr>
-           <td>3</td>
-           <td>Hyundai</td>
-           <td>Elantra</td>
-           <td>2010</td>
-       </tr>
-       </tbody>
-       </table>
-
-       <br> </br>
-       <br> </br>
-
-      <form class="main" action = "main.php" method = "post">
-         <button>Back</button>
-      </form>
-   </div>
+               <br> </br>
+               <br> </br>
+               <br> </br>
+               <br> </br>
 
 
 
-</div>
-</body>
-</html>
+               <form class="main" action = "main.php" method = "post">
+                   <button>Back</button>
+               </form>
+           </div>
+       </div>
+       </body>
+       </html>
